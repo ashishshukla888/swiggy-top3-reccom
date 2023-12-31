0c2992d9d1a7f40b76776283c85b2c2523739f69
ashishshukla888 <ashishshukla888.ak@gmail.com>
2023-12-31 08:02:35 +0530

    DVC

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project help reccomend top 3 food items on swiggy',
+    author='ashish shukla',
+    license='',
+)
